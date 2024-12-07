a7c2a119f6eef1094a62fa8aae36df5c618a2168
Emanuel Vogt <emanuel.vogt@pixit.com.br>
2024-12-07 15:51:55 -0300

    feat: create a list of widgets from transactions

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import './Models/transaction.dart'; // Adicionado ponto-e-vírgula
 
 void main() {
   runApp(const MyApp());
@@ -14,18 +15,24 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
         appBarTheme: const AppBarTheme(
-          backgroundColor:
-              Colors.blue, // Explicitly set AppBar background color
-          foregroundColor: Colors.white, // Ensure text/icons are visible
+          backgroundColor: Colors.blue, // Explicitamente definindo cor
+          foregroundColor: Colors.white, // Garantindo visibilidade do texto/ícones
         ),
       ),
-      home: const MyHomePage(),
+      home: MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatelessWidget {
-  const MyHomePage({super.key});
+  MyHomePage({super.key}); 
+
+  final _transactions = [
+    Transaction(
+        id: '1', title: 'Nova transaction', value: 2.2, date: DateTime.now()),
+    Transaction(
+        id: '2', title: 'Outra transaction', value: 3.5, date: DateTime.now())
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -33,21 +40,25 @@ class MyHomePage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Standard Blue AppBar'),
       ),
-      body: const Column(
+      body: Column( 
         mainAxisAlignment: MainAxisAlignment.spaceAround,
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
-          SizedBox(
-              width: double.infinity,
-              child: Card(
-                color: Colors.blue,
-                elevation: 5,
-                child: Text('Grafico'),
-              )),
-          Card(
-            elevation: 5,
-            child: Text('lista de transacoes'),
-          )
+          const SizedBox(
+            width: double.infinity,
+            child: Card(
+              color: Colors.blue,
+              elevation: 5,
+              child: Text('Gráfico'),
+            ),
+          ),
+          Column(
+            children: _transactions.map((tr) {
+              return Card(
+                child: Text(tr.title),
+              );
+            }).toList(),
+          ),
         ],
       ),
     );
